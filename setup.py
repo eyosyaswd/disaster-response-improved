41fad89af864d5c34a808eef5838e058967b1980
Eyosyas Dagnachew <eyosyaswd@outlook.com>
2021-12-05 12:32:36 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Attempt to improve the results from the paper 'Analysis of Social Media Data using Multimodal Deep Learning for Disaster Response'.',
+    author='Eyosyas Dagnachew',
+    license='',
+)
